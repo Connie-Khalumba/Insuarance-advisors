78bb17043cd7fdeebe57d77f07948956efb17442
constance khalumba <constancekhalumba@gmail.com>
2024-09-03 09:46:02 +0300

    installed vite and dependancies

--- auth.js
+++ auth.js
@@ -1,27 +1,27 @@
-// require('dotenv').config();
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBt8E_ET9F_uaT99D2kBlREpaPCNOYBJVI",
-    authDomain: "health-insurance-2076a.firebaseapp.com",
-    projectId: "health-insurance-2076a",
-    storageBucket: "health-insurance-2076a.appspot.com",
-    messagingSenderId: "1039106417461",
-    appId: "1:1039106417461:web:f5697ba0ed02dd8803227c",
-    measurementId: "G-B2TPR7EY86"
-  };
-  
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
-  const auth = getAuth();
+  apiKey: "AIzaSyBt8E_ET9F_uaT99D2kBlREpaPCNOYBJVI",
+  authDomain: "health-insurance-2076a.firebaseapp.com",
+  projectId: "health-insurance-2076a",
+  storageBucket: "health-insurance-2076a.appspot.com",
+  messagingSenderId: "1039106417461",
+  appId: "1:1039106417461:web:f5697ba0ed02dd8803227c",
+  measurementId: "G-B2TPR7EY86"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider()
+
   
   const submitButton = document.getElementById("submit");
   
@@ -41,6 +41,7 @@ const firebaseConfig = {
   const createacctbtn = document.getElementById("create-acct-btn");
   const returnBtn = document.getElementById("return-btn");
   const signupButton = document.getElementById("sign-up");
+  const signoutButton = document.getElementById("sign-out");
   
   var email,
     password,
